74c7087365b09fb6da47035b69530c4ba2c9c172
Menhazul Abdin <menhazul12345@gmail.com>
2026-04-17 00:47:37 +0530

    Add allowed hosts to Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    allowedHosts: [
+      'altitudess.onrender.com'
+    ],
     proxy: {
       // '/api': 'http://localhost:3000'
       '/api':{
